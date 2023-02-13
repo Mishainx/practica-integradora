8630ec591cf07cff9d48630f4bb0565013835a48
fernando <fer.r@live.com.ar>
2023-02-13 08:33:13 -0300

    modificaciones

--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -30,9 +30,54 @@ routerViews.get('/realTimeProducts', async (req,res)=>{
 // RouterViews.GET "Products" devuelve una vista  del listado de productos con paginación
 routerViews.get('/products', async (req,res)=>{
     try{
-        const productsList = await productModel.paginate({},{lean:true,page:1,limit:1})
-        console.log(productsList)     
-        res.status(200).render('products',{styleSheets:'css/styles',productsList})
+        const limit = req.query.limit || 10
+        const page = req.query.page || 1
+        let category = req.query.category || undefined
+        const sort = req.query.sort || ""
+        let stockQuery = req.query.stock || undefined
+    
+        //Validación en caso de que se haya ingresado limit
+        if(isNaN(Number(limit))){
+          res.status(400).send({status:"error", payload:"El limit ingresado es inválido"})
+          return
+        }
+        //Validación en caso de que se haya ingresado page
+        if(isNaN(Number(page))){
+          res.status(400).send({status:"error", payload:"El valor de page inválido"})
+          return
+        }
+    
+        //Validación en caso de que se haya ingresado sort
+        if(sort != ""){
+          if(sort != 1 & sort !=-1){
+            res.status(400).send({status:'error',payload:'El valor de sort debe ser 1 o -1'})
+            return
+          }
+        }
+    
+        //Validación en caso de que se haya ingresado query por stock
+        if(stockQuery!=undefined){
+          if(stockQuery !=1 & stockQuery!=0){
+            res.status(400).send({status:"Error",payload:"El valor ingresado es incorrecto. Sin stock:0/ Con stock:1"})
+            return
+          }
+          stockQuery==1?stockQuery={stock:{$gte:1}}:stockQuery={stock:{$lt:1}}
+        }
+    
+            //Validación en caso de que se haya ingresado query por categoría
+            if(category!=undefined){
+              const checkCategory = await productModel.exists({category:category})
+              if(!checkCategory){
+                res.status(400).send({status:"Error",payload:"La categoría ingresada es inexistente"})
+                return
+              }
+              category={category:category}
+            }
+    
+        // Se realiza la paginación conforme los querys seleccionados
+        let productlist = await productModel.paginate({...category,...stockQuery},{lean:true, limit:limit, page:page, sort: {price:sort}})
+
+        res.status(200).render('products',{styleSheets:'css/styles', productlist})
     }
     catch(err){
         res.status(500).send({error:err})
@@ -57,7 +102,6 @@ routerViews.get("/carts/:cid",async (req,res)=>{
   
       //Comprobación de la existencia de un carrito con el parámetro ingresado.
       const cartExist = await cartModel.findById(cartId).lean().populate("products.product")
-      console.log(cartExist)
       if(cartExist==null){
         let message = "No existe un carrito con la Id seleccionada"
         res.status(400).render('carts_Id',{title:"Cart Id",styleSheets:'css/styles', message})
